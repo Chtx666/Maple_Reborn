26b953870bcc4f256ea940545f1ec5c0d3bedac6
Guashark <221940752+Guashark@users.noreply.github.com>
2026-05-16 21:55:10 +0800

    2026/5/16 - 1 记得看doc

--- Scripts/mainMenu.gd
+++ Scripts/mainMenu.gd
@@ -1,11 +1,11 @@
 extends Control
 
-var messageBox 
+var quit_popup 
 	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	messageBox = get_node("Confirmation")
-	messageBox.visible = false
+	quit_popup = get_node("QuitPopup")
+	quit_popup.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -21,10 +21,10 @@ func _on_setting_btn_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/TestScene.tscn")
 
 func _on_quit_btn_pressed() -> void:
-	messageBox.visible = true
+	quit_popup.visible = true
 
-func _on_cancel_pressed() -> void:
-	messageBox.visible = false
-	
-func _on_confirm_pressed() -> void:
+func _on_confirm_btn_pressed() -> void:
 	get_tree().quit()
+
+func _on_cancel_btn_pressed() -> void:
+	quit_popup.visible = false
